007a180b5cdf56641a1fd6980fb204bae3b09381
Huang Chung YI <cyh@hcytlog.com>
2025-11-07 00:22:04 +0800

    Update Homebrew cask to v1.3.2

--- Casks/termdock.rb
+++ Casks/termdock.rb
@@ -1,11 +1,11 @@
 cask "termdock" do
   arch arm: "arm64", intel: "x64"
   
-  version "1.3.1"
-  sha256 arm:   "c61b581ab34312e54a31994a596763edfda1c5e3f2726f03f1502b60a6e0c037",
-         intel: "800da4eef33abbebcb2be28f5f9a98e4fadf08cb4c8a94eaf125017d7f1c3953"
+  version "1.3.2"
+  sha256 arm:   "9f4fefed745e9c847244de806f5fe1a1494e07e6d2d91d26509319c06feeccfe",
+         intel: "774ef6ccae8ff2b8cb46337cc29aa9c4903eb79a2d11778ec39cf9db8940d639"
 
-  url "https://github.com/termdock/Termdock-issues/releases/download/v1.3.1/Termdock-#{version}#{arch == :intel ? "" : "-arm64"}.dmg"
+  url "https://github.com/termdock/Termdock-issues/releases/download/v1.3.2/Termdock-#{version}#{arch == :intel ? "" : "-arm64"}.dmg"
   name "Termdock"
   desc "AI-Powered Terminal Integration Platform"
   homepage "https://github.com/termdock/Termdock-issues"

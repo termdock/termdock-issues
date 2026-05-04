4d04308b4fd44e03dd830fa7fab530e8f45c0d91
cyh7789 <catowner7@gmail.com>
2026-05-04 16:05:23 +0800

    Update Homebrew cask to v1.8.1

--- Casks/termdock.rb
+++ Casks/termdock.rb
@@ -1,11 +1,11 @@
 cask "termdock" do
   arch arm: "arm64", intel: "x64"
   
-  version "1.8.0"
-  sha256 arm:   "8d2700979a5d2cdf82aad75aae7f922b41076e1ae53e321db32ad5db6c69a7b1",
-         intel: "169936f4c79769d151f0614f4ae81135f39f27dd6d9b4868844dfab0d22e9337"
+  version "1.8.1"
+  sha256 arm:   "598217fed856c21fd2bcbdaa84fd18ea4b4be2512fba5ffdbb76d2b3e1b38b83",
+         intel: "e5cbe05a9c1736d1de835e7e1f07d995c8ba6b352362f88a965d8eb063ee9ff8"
 
-  url "https://github.com/termdock/Termdock-issues/releases/download/v1.8.0/Termdock-#{version}#{arch == :intel ? "" : "-arm64"}.dmg"
+  url "https://github.com/termdock/Termdock-issues/releases/download/v1.8.1/Termdock-#{version}#{arch == :intel ? "" : "-arm64"}.dmg"
   name "Termdock"
   desc "AI-Powered Terminal Integration Platform"
   homepage "https://github.com/termdock/Termdock-issues"
